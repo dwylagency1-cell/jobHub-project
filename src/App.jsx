1b73956c4ac1a15597f78419079f1b7a345081c5
Dekiru <dwyl.agency1@gmail.com>
2026-07-07 12:43:51 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from './components/Navbar.jsx'
+import Middle from './components/middle.jsx'
+function App() {
+  return <div>
+    <Navbar></Navbar>
+    <Middle></Middle>
+  </div>
+}
+
+export default App
