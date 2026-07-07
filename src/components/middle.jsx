20bf53eaa5615267f9ab6f976f055b52a79d098c
Dekiru <dwyl.agency1@gmail.com>
2026-07-07 12:47:17 +0530

    middle.jsx

---
+++ src/components/middle.jsx
@@ -0,0 +1,19 @@
+function Middle() {
+    return <div>
+        <div className="  flex h-[580px] justify-between items-center ">
+            <div className=" h-[580px] w-[670px] flex flex-col items-center justify-center p-[70px]">
+                <div className="flex bg-pink-300 h-[580px] w-[600px] items-center justify-center gap-x-[20px] flex-col gap-y-[20px]">
+                    <div className="bg-blue-200 h-[200px] w-[600px]">
+                        <h1 className="text-[50px] font-semibold font-[arial] mt-[70px] text-gray-800">Find Your Dream job with us</h1>
+                    </div>
+                    <div className="bg-black h-[250px] w-[600px]"></div>
+
+                </div>
+
+            </div>
+            <div className="bg-red-500 h-[580px] w-[670px]"></div>
+
+        </div>
+    </div>
+}
+export default Middle
