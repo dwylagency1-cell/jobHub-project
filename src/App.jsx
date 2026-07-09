89743f7d0aba08d2c709a2a38b917d7ad27fa75b
Dekiru <dwyl.agency1@gmail.com>
2026-07-09 13:36:58 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Navbar from './components/Navbar.jsx'
 import Middle from './components/middle.jsx'
+import { Routes, Route } from "react-router-dom"
+import Form from './components/form.jsx'
 function App() {
+  
   return <div>
+    
     <Navbar></Navbar>
     <Middle></Middle>
+    
   </div>
 }
 
