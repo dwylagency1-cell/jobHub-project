d3ff0ebfcf4115bff17b304dcef7f7dadf808fe5
Dekiru <dwyl.agency1@gmail.com>
2026-07-16 13:01:26 +0530

    form.jsx

---
+++ src/pages/form.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import logo from './img/jobhub_logo.png'
+import Dashboard from './dashboard.jsx'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+function Form() {
+    const navigate = useNavigate()
+    function handelSubmit(e) {
+        e.preventDefault()
+        console.log(email)
+        console.log(first)
+        console.log(last)
+        setEmail('')
+        setFirst('')
+        setLast('')
+
+        navigate('/dashboard')
+    }
+
+
+    const [email, setEmail] = useState('')
+    const [first, setFirst] = useState('')
+    const [last, setLast] = useState('')
+
+    return <div className="h-screen w-full bg-[#F8FAFC] flex justify-center items-center">
+        <div >
+            <form onSubmit={handelSubmit} className="bg-white flex flex-col justify-center items-center gap-y-[20px] p-[100px] rounded-[20px]">
+                <img className='w-[200px]' src={logo}></img>
+                <div className="flex justify-between items-center gap-x-[10px]">
+                    <input className="w-[200px] pt-[10px] pb-[10px] border-solid border rounded-[10px] pl-[5px]" type="text" name='first_name' placeholder="First name" value={first} onChange={function(e){
+                        setFirst(e.target.value);
+                    }}></input>
+                    <input className="w-[200px] pt-[10px] pb-[10px] border-solid border rounded-[10px] pl-[5px]" type="text" name='last_name' placeholder="Last name" value={last} onChange={function(e) {
+                        setLast(e.target.value);
+                    }}></input>
+                </div>
+                <input className="w-[400px] pt-[10px] pb-[10px] border-solid border rounded-[10px] pl-[5px]" type='email' placeholder="Email" value={email} onChange={function(e) 
+                    {setEmail(e.target.value);}}></input>
+                <button type="submit" className="w-[400px] pt-[10px] pb-[10px] border-solid border rounded-[10px] bg-green-400 text-[white] cursor-pointer">Submit</button>
+            </form>
+        </div>
+    </div>
+}
+export default Form
