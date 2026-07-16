e6d4300152751556c8eca253fd3f4349970daac0
Dekiru <dwyl.agency1@gmail.com>
2026-07-16 13:00:44 +0530

    Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar.jsx'
+import Middle from '../components/middle.jsx'
+function Home() {
+    return <div>
+        <Navbar></Navbar>
+        <Middle></Middle>
+
+    </div>
+}
+export default Home
