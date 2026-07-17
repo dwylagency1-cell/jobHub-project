eecf9fea8918c2b2f32f02da005ca3aa25c3f05c
Dekiru <dwyl.agency1@gmail.com>
2026-07-17 14:22:23 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
+import logo from '../pages/img/joblogo.png'
+import { useNavigate } from 'react-router-dom'
 function Navbar() {
+    const navigate =  useNavigate()
     return <div>
         <div className="flex  h-[70px] justify-between item-center">
             <div className="flex  justify-center items-center w-[300px] h-[70px]">
-                <h1 className="text-[30px]">JobHub</h1>
+                <img className='w-[250px]' src={logo}></img>
 
             </div>
             <div className="flex  justify-between items-center w-[400px] h-[70px] font-[arial] text-gray-600 ">
@@ -12,7 +16,9 @@ function Navbar() {
 
             </div>
             <div className="flex justify-center items-center w-[300px] h-[70px]">
-                <button className="pt-[10px] pb-[10px] pr-[30px] pl-[30px] bg-green-400 rounded-[10px] text-[white]">Register</button>
+                <button onClick={function(){
+                    navigate('/register')
+                }} className="pt-[10px] pb-[10px] pr-[30px] pl-[30px] bg-green-400 rounded-[10px] text-[white] cursor-pointer">Register</button>
             </div>
 
 
