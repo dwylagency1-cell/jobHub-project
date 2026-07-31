e800d467a19df636ed8e87edf510638a18e10835
Dekiru <dwyl.agency1@gmail.com>
2026-07-31 17:29:51 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Form from "./pages/form.jsx";
 import Dashboard from "./pages/dashboard.jsx";
+import Profile from './pages/profile.jsx'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Form />} />
       <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="/profile" element={<Profile></Profile>}></Route>
     </Routes>
   );
 }
